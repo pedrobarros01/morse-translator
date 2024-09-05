5b225ddf602f59993944edcff7c975a97d20c0ca
Pedro Barros <82074537+pedrobarros01@users.noreply.github.com>
2024-09-05 15:10:33 -0300

    Colocando comunicação

--- ard1.cpp
+++ ard1.cpp
@@ -1,6 +1,5 @@
 int botao = 3;
-int modoTransmissao = 2;  // Modo de transmissão inicial
-bool botaoPressionado = false;  // Variável para garantir que o botão só seja considerado uma vez
+int modoTransmissao = 0;  
 char letras[] = {'A', 'B', 'C', 'D', 
                  'E', 'F', 'G', 'H', 
                  'I', 'J', 'K', 'L', 
@@ -12,54 +11,53 @@ char letras[] = {'A', 'B', 'C', 'D',
 String morse[1] = {
 "1"
 };
-String valorMorse = "1000";
-
+String valorMorse = "";
+int contarZeros = 0;
 void setup() {
   pinMode(botao, INPUT);
   Serial.begin(9600);
-  modoTransmissao = 2;
+  modoTransmissao = 0;
 }
 
 void loop() {
   int leituraBotao = digitalRead(botao);
 
-  // Se o botão for pressionado e ainda não foi considerado
-  if (leituraBotao == HIGH && !botaoPressionado && modoTransmissao == 2) {
-    Serial.write("ARD1");  // Envia mensagem ARD1
-    botaoPressionado = true;  // Marca o botão como pressionado
-    modoTransmissao = 1;  // Altera para o modo de envio de mensagem
-  }
-
-  // Verifica se recebeu uma mensagem e está no modo de espera (modo 2)
-  if (Serial.available() > 0 && modoTransmissao == 2) {
+  if (Serial.available() > 0 ) {
     String c = Serial.readString();
     Serial.println(c);
-    //Serial.end();
-    if (c == "ARD2") {  // Se receber "ARD2" do Arduino 2
-      modoTransmissao = 0;  // Vai para o modo de tradução de mensagem
+    delay(2000);
+    clearSerial();
+  }
+  if(leituraBotao == HIGH && !modoTransmissao){
+      modoTransmissao = 1;
+   }
+   if(modoTransmissao){
+    if(leituraBotao == LOW && contarZeros < 3){
+      valorMorse += "0";
+      contarZeros++;
+    }else if(leituraBotao == HIGH && contarZeros < 3){
+      valorMorse += "1";
+      contarZeros = 0;
+    }
+  
+  if(contarZeros == 3){
+      char letraTraduzida = tradutorMorse(valorMorse);
+      valorMorse = "";
+      modoTransmissao = 0;
+      String aoba5 = "Letra Traduzida ARD2: " + String(letraTraduzida);
+      Serial.print(aoba5 + "\n");
+      delay(2000);
+      clearSerial();
     }
   }
-
-  // Se estiver no modo de tradução (modo 0)
-  if (modoTransmissao == 0 && Serial.available() > 0) {
-    String c = Serial.readString();
-    Serial.print(c);  // Mostra a mensagem recebida
-    //delay(1000);
-    modoTransmissao = 2;  // Retorna ao modo de espera
-  }
-
-  // Se estiver no modo de envio da mensagem traduzida (modo 1)
-  if (modoTransmissao == 1) {
-    char letraTraduzida = tradutorMorse(valorMorse);  // Traduz a mensagem Morse
-    //Serial.write(letraTraduzida);  // Envia a letra traduzida
-    valorMorse = "";  // Limpa o valor Morse
-    modoTransmissao = 2;  // Volta ao modo original
-    botaoPressionado = false;  // Libera para outra leitura do botão
-  }
-
-  delay(1000);  // Pequeno atraso para evitar leituras rápidas demais
+  delay(1000);
+ 
 }
-
+void clearSerial() {
+  while (Serial.available() > 0) {
+    Serial.read();  
+  }
+}
 char tradutorMorse(String valor) {
   int tamanho = 1;
   String aux = "";
@@ -71,5 +69,5 @@ char tradutorMorse(String valor) {
       return letras[i];
     }
   }
-  return '0';  // Retorna '0' se a tradução falhar
+  return '0';  
 }
